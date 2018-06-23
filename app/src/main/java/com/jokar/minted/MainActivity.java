959be47ce9bdc8a521b6cdf6afc3c59547fb603b
jokar13 <wivekks@gmail.com>
2018-06-23 17:54:32 +0530

    Initial Commit| Modified

---
+++ app/src/main/java/com/jokar/minted/MainActivity.java
@@ -0,0 +1,16 @@
+package com.jokar.minted;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Started", Toast.LENGTH_SHORT).show();
+    }
+}
